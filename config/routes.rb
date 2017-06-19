f7c416c492bf93d3db5e5019a159852b6932a7b7
Minh Huynh <minhh1990@gmail.com>
2017-06-18 20:29:55 -0700

    added new route and controller action for edit password reset

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#show'
   resources :users, except: [:destroy]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :password_resets, only: [:new, :create]
+  get 'password_resets/:token', as: "edit_password_reset", to: "password_resets#edit"
 end
