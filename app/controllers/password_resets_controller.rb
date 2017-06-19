86a51244a867debe2bb9032d1c8c28e0e0b1c9bb
Minh Huynh <minhh1990@gmail.com>
2017-06-18 19:47:56 -0700

    added styling to password reset page. Added file to .gitignore

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,10 +3,19 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    url_string = SecureRandom.urlsafe_base64
-    user.reset_token = url_string
-    user.save
-    PasswordResetMailer.password_reset_email(user).deliver
+    @user = User.find_by(email: params[:email])
+    if @user
+      flash[:notice] = "An email has been sent with a link to reset your password"
+      url_string = SecureRandom.urlsafe_base64
+      @user.reset_token = url_string
+      @user.save
+      PasswordResetMailer.password_reset_email(@user).deliver
+      redirect_to new_session_path
+    else
+      flash[:error] = "There was an error in your submission"
+      render :new
+    end
   end
+
+
 end
