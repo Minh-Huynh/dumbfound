49af88459c401582eca380d43deda2d9c522e675
Minh Huynh <minhh1990@gmail.com>
2017-06-27 20:10:12 -0700

    added message service object to handle texting. Added text formatting, enumeration, sending texts in chunks of three steps.

--- app/services/direction.rb
+++ app/services/direction.rb
@@ -1,28 +1,31 @@
 class Direction
  attr_accessor :token, :domain, :origin, :destination, :time
 
-  def initialize(origin:, destination:, time:)
+  def initialize
     @token = ENV["google_maps_token"]
     @domain = "https://maps.googleapis.com/maps/api/directions"
-    @origin = origin
-    @destination = destination
-    @time = time
   end
 
-  def make_request
+  def make_request(origin:, destination:, time:)
     connection = Faraday.new domain do |conn|
       conn.response :json, :content_type => /\bjson$/
       conn.adapter Faraday.default_adapter
     end
-    response = connection.post(query_string)
-    byebug
-    directions = response.body["routes"].first["legs"].first["steps"].map do |step|
-      ActionController::Base.helpers.strip_tags(step["html_instructions"])
-    end
-    directions
+    response = connection.post(query_string(origin: origin, destination: destination, time: time))
   end 
 
-  def query_string
+  def self.process_and_format(response, steps_per_chunk)
+    directions = response.body["routes"].first["legs"].first["steps"].map.with_index do |step, index|
+      ActionController::Base.helpers.strip_tags(step["html_instructions"]).prepend("#{index}) ")
+    end
+    directions.in_groups_of(steps_per_chunk)
+  end
+
+  def query_string(origin:, destination:,time:)
 		"json?origin=#{origin}&destination=#{destination}&departure_time=#{time}&traffic_model=best_guess&key=#{token}"
+  end
+
+  def self.valid_request?(response)
+    response.body["status"] == "OK"
   end
 end
