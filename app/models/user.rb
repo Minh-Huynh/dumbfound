19b9303adec6924b78c640552c9f6a2336e0e16b
Minh Huynh <minhh1990@gmail.com>
2017-07-02 13:03:42 -0700

    added validations to user model to prevent multiple common users and such

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :email
   validates_presence_of :phone_number
+  validates_uniqueness_of :email
+  validates_uniqueness_of :phone_number
   before_save :format_phone_number
 
   def format_phone_number
-    self.phone_number.gsub! /[^\d]/, ""
+    self.phone_number.gsub! /[^\d]/, "" if phone_number
   end
 end
