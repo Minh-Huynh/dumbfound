f8517290bb0be1867f40218427e614f3b9880db7
Minh Huynh <minhh1990@gmail.com>
2017-07-01 17:59:43 -0700

    added tests for messages controller. Also added error messages and ability to specify transit mode

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,28 +2,33 @@ class MessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
   require 'faraday_middleware'
 
+  STEPS_PER_SMS_MSG = 3
+
   def reply
     formatted_phone_number = params[:From].gsub /^\+1/,""
     user = User.find_by(phone_number: formatted_phone_number)
+    received_msg = params[:Body]
     
-    if user
-      received_msg = params[:Body]
-      from, to = parse_incoming_message(received_msg)
-      directions = Direction.new
-      response = directions.make_request(origin: from, destination: to, time: Time.now.getutc.to_i)
-      if Direction.valid_request?(response)
-        Direction.process_and_format(response, 3).each do |steps|
+    if user && received_msg =~ / to /
+      from, to, travel_mode = parse_incoming_message(received_msg)
+      directions = Direction.new(origin: from, destination: to, time: Time.now.getutc.to_i, travel_mode: travel_mode)
+      directions.make_request
+      if directions.valid_request?
+        directions.process_and_format(STEPS_PER_SMS_MSG).each do |steps|
           Message.new.send_message(params[:From],steps.join(" "))
         end
       end
+    elsif user
+      Message.new.send_message(params[:From], "Invalid Request: Please reformat to <origin> to <destination>")
     end
   end
 
   private
   def parse_incoming_message(msg)
-    locations = msg.split("to")
+    locations = msg.split(/to | by/)
+    mode = locations.count == 3 ? locations[2] : nil
     from = locations[0].gsub(/\s+/, "+").chomp("+")
     to = locations[1].gsub(/\s+/, "+")
-    return from,to
+    return from,to, mode
   end
 end
--- app/services/direction.rb
+++ app/services/direction.rb
@@ -1,31 +1,41 @@
 class Direction
- attr_accessor :token, :domain, :origin, :destination, :time
+ attr_accessor :token, :domain, :origin, :destination, :time, :travel_mode, :traffic_model
 
-  def initialize
+  def initialize(origin:, destination:, time:, travel_mode:)
     @token = ENV["google_maps_token"]
     @domain = "https://maps.googleapis.com/maps/api/directions"
+    @origin = origin
+    @destination = destination
+    @time = time
+    @traffic_model = "best_guess"
+    @travel_mode = travel_mode
   end
 
-  def make_request(origin:, destination:, time:)
+  def make_request
     connection = Faraday.new domain do |conn|
       conn.response :json, :content_type => /\bjson$/
       conn.adapter Faraday.default_adapter
     end
-    response = connection.post(query_string(origin: origin, destination: destination, time: time))
+    @response = connection.post(query_string(origin: origin,
+                                             destination: destination,
+                                             time: time,
+                                             travel_mode: travel_mode))
   end 
 
-  def self.process_and_format(response, steps_per_chunk)
-    directions = response.body["routes"].first["legs"].first["steps"].map.with_index do |step, index|
-      ActionController::Base.helpers.strip_tags(step["html_instructions"]).prepend("#{index}) ")
+  def process_and_format(steps_per_chunk)
+    if @response
+      directions = @response.body["routes"].first["legs"].first["steps"].map.with_index do |step, index|
+        ActionController::Base.helpers.strip_tags(step["html_instructions"]).prepend("#{index}) ")
+      end
+      directions.in_groups_of(steps_per_chunk)
     end
-    directions.in_groups_of(steps_per_chunk)
   end
 
-  def query_string(origin:, destination:,time:)
-		"json?origin=#{origin}&destination=#{destination}&departure_time=#{time}&traffic_model=best_guess&key=#{token}"
+  def query_string(origin:, destination:,time:, travel_mode:)
+		"json?origin=#{origin}&destination=#{destination}&departure_time=#{time}&traffic_model=#{traffic_model}&travel_mode=#{travel_mode}&key=#{token}"
   end
 
-  def self.valid_request?(response)
+  def valid_request?
     response.body["status"] == "OK"
   end
 end
