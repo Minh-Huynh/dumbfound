cec95b7a98816fce51ceebbb81aaa64db978d571
Minh Huynh <minhh1990@gmail.com>
2017-07-07 14:34:00 -0700

    added phone number to welcome email and changed subject line

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -3,6 +3,6 @@ default from: 'noreply@dumbfound.com'
 
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: "Test Email")
+    mail(to: @user.email, subject: "Welcome to DumbFound!")
   end
 end
