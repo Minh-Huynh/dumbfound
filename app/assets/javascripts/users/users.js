bd03d8b59b75e16ead292bca2bf08be94af75fc0
Minh Huynh <minhh1990@gmail.com>
2017-07-08 12:58:58 -0700

    added code to bind Jquery with turbolinks so phone number masking occurs

--- app/assets/javascripts/users/users.js
+++ app/assets/javascripts/users/users.js
@@ -1,3 +1,8 @@
-$(function() {
-				$(".user_phone").mask("(999)999-9999", {placeholder: "(xxx)xxx-xxxx"});
-});
+var ready;
+ready = function() {
+				$("#user_phone_number").mask("(999)999-9999", {placeholder: "(xxx)xxx-xxxx"});
+};
+
+$(document).ready(ready);
+$(document).on('turbolinks:load', ready);
+
